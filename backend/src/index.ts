216f73531a13c45971c836f4eac0b29e9c88d665
ubiratann <r14.ubiratanjunior@gmail.com>
2021-09-25 12:02:06 -0300

    [+] configuracao basica do servidor

---
+++ backend/src/index.ts
@@ -0,0 +1,27 @@
+import * as dotenv from "dotenv"
+import express from "express"
+import cors from "cors"
+import helmet from "helmet"
+
+
+dotenv.config();
+
+
+if(!process.env.PORT)
+{
+    process.exit(1);
+}
+
+const PORT: number = parseInt(process.env.PORT);
+
+const app = express();
+
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+
+app.listen(PORT, ()=>{
+  console.log(`SERVIDOR INICADO NA PORTA ${PORT}`);  
+});
